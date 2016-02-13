aaeff35733db380e0872d34e6d88355c0fd49ba0
Twllerm <skyteether@gmail.com>
2016-02-13 14:16:23 +0300

    packajes

---
+++ Front-end/WebApp/db/mongoose.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var config = require('../config');
+var schema = mongoose.Schema;
+
+mongoose.connect(config.mongooseString);
+
+var student = mongoose.model('Student', {
+	firstName: String,
+	lastName: String,
+	middleName: String,
+	department: String,
+	group: String,
+	course: Number,
+	email: String,
+	password: String,
+	phone: String,
+	about: String,
+	avatarUri: String,
+	achivments: [{name: String, type: String}]
+})
