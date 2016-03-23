3f2f5acc0a60b1e0f673aee03d88825dad0b9728
Twllerm <skyteether@gmail.com>
2016-03-23 16:15:13 +0300

    ss

--- Front-end/WebApp/api/achivments.js
+++ Front-end/WebApp/api/achivments.js
@@ -12,6 +12,7 @@ module.exports = {
   	getAchivmentDetail: getAchivmentDetail,
   	newAchivment: newAchivment,
     getAchivmentsList: getAchivmentsList,
+    addFile: addFile
 }
 
 function getAchivmentsList(req, res, next) {
@@ -30,29 +31,95 @@ function getAchivmentDetail(req, res, next) {
  })
 };
 
+function addFile(req, res, next) {
+    var form = new multiparty.Form();
+    var file = {};
+    var uploadFile = {uploadPath: '', type: '', size: 0},
+    maxSize = 2 * 1024 * 1024,
+    supportMimeTypes = config.types,
+    errors = [];
+
+    form.on('close', function() {
+        if(errors.length == 0) {
+    
+         Student.findOne({'achivments._id': req.params.ach_id}, 'achivments.$', function(err, data) {
+            if(err) {
+                res.send(err);
+            } else {
+                console.log(data);
+            }
+         })
+           
+        }
+        else {
+            if(fs.existsSync(uploadFile.path)) {
+                fs.unlinkSync(uploadFile.path);
+            }
+            res.send({status: 'bad', errors: errors});
+        }
+    });
+
+      form.on('error', function(err){
+        if(fs.existsSync(uploadFile.path)) {
+            fs.unlinkSync(uploadFile.path);
+            res.send('error');
+        }
+
+    });
+
+      form.on('part', function(part) {
+        if(part.filename) {
+        console.log(part.filename);
+        uploadFile.size = part.byteCount;
+        uploadFile.type = part.headers['content-type'];
+        var fileNameHash = crypto.createHmac('sha256', 'ach').update(part.filename).digest('hex').slice(10);
+        uploadFile.path = config.path + req.params.student_id + '/' + fileNameHash;
+        uploadFile.link = config.link + req.params.student_id + '/' + fileNameHash;
+
+        //проверяем размер файла, он не должен быть больше максимального размера
+        if(uploadFile.size > maxSize) {
+            errors.push('File size is ' + uploadFile.size + '. Limit is' + (maxSize / 1024 / 1024) + 'MB.');
+        }
+
+        //проверяем является ли тип поддерживаемым
+        if(supportMimeTypes.indexOf(uploadFile.type) == -1) {
+            errors.push('Unsupported mimetype ' + uploadFile.type);
+        }
+
+        //если нет ошибок то создаем поток для записи файла
+        if(errors.length == 0) {
+            var studentFolder = config.path + req.params.student_id;
+            if (!fs.existsSync(studentFolder)) {
+                fs.mkdirSync(studentFolder);
+            }
+            var out = fs.createWriteStream(uploadFile.path);
+            part.pipe(out);
+            file = uploadFile.link;
+        }
+        else {
+            //пропускаем
+            //вообще здесь нужно как-то остановить загрузку и перейти к onclose
+            part.resume();
+        }
+           }
+    });
+
+
+}
+
 function newAchivment(req, res, next) {
 	
  
   var form = new multiparty.Form();
 
-    var uploadFile = {uploadPath: '', type: '', size: 0},
-     maxSize = 2 * 1024 * 1024,
-     supportMimeTypes = config.types,
-     errors = [];
-
+  
     var achivment = {
         checked: false,
         files: [],
         updated: new Date()
     }
 
-    form.on('error', function(err){
-        if(fs.existsSync(uploadFile.path)) {
-            fs.unlinkSync(uploadFile.path);
-            res.send('error');
-        }
-    });
-
+   
     form.on('close', function() {
         if(errors.length == 0) {
     
@@ -80,6 +147,7 @@ function newAchivment(req, res, next) {
             }
             res.send({status: 'bad', errors: errors});
         }
+
     });
 
     form.on('part', function(part) {
--- Front-end/WebApp/public/js/build/build.js
+++ Front-end/WebApp/public/js/build/build.js
@@ -684,7 +684,7 @@ angular.module('app.services', [])
         return {
             Current: Current,
             logout: logout,
-           update: update
+            update: update
         }
     }])
 
